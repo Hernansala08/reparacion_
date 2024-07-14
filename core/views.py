9c3efabdf35c90e44e84188b685d60c0e5d4c06c
Pokerus <patricio.288920@gmail.com>
2024-07-14 22:08:01 +0200

    Correcciones

--- core/views.py
+++ core/views.py
@@ -3,19 +3,19 @@ from .models import Celular, Laptop, PC
 
 # Create your views here.
 def home(request):
-    return render(request, 'pages/home.html')
+    return render(request, 'core/home.html')
 
 def more(request):
-    return render(request, 'pages/more.html')
+    return render(request, 'core/more.html')
 
 def celulares(request):
     Cel = Celular.objects.all()
-    return render(request, 'pages/more.html',{'Celulares':Cel})
+    return render(request, 'core/more.html',{'Celulares':Cel})
 
 def laptops(request):
     Lap = Laptop.objects.all()
-    return render(request, 'pages/more.html',{'Laptops':Lap})
+    return render(request, 'core/more.html',{'Laptops':Lap})
 
 def pcs(request):
     Pc = PC.objects.all()
-    return render(request, 'pages/more.html',{'PCs':Pc})
+    return render(request, 'core/more.html',{'PCs':Pc})
